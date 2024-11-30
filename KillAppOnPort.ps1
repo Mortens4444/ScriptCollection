19e3731f1abbb0e75bf35dc56b7f739dac0f5529
Mortens <mortens.4444@gmail.com>
2024-11-30 11:45:41 +0100

    Kill application on port

---
+++ KillAppOnPort.ps1
@@ -0,0 +1,21 @@
+﻿# Search for PID, ehich uses port 4200
+$port = 4200
+$processId = netstat -ano | findstr ":$port" | ForEach-Object { 
+    $columns = $_ -split '\s+'
+    if ($columns[4] -match '^\d+$') { $columns[4] }
+}
+
+if ($processId) {
+    Write-Output "PID found: $processId"
+
+    # Check if it not our PID
+    if ($processId -ne $PID) {
+        # Kill process
+        taskkill /PID $processId /F
+        Write-Output "Process $processId terminated."
+    } else {
+        Write-Output "The script's own process PID ($PID) matches the target PID. Not terminating."
+    }
+} else {
+    Write-Output "No process found on port $port."
+}
