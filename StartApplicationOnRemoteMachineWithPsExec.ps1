52570eae45d6bd60b2db3fa9b6cd18b45b554119
Mortens <mortens.4444@gmail.com>
2025-03-30 14:23:31 +0200

    Start application on remote machine with PsExec

---
+++ StartApplicationOnRemoteMachineWithPsExec.ps1
@@ -0,0 +1,12 @@
+﻿$username = "user"
+$password = "pass"
+$machine = "machine_name"
+$LiveViewAgentLocation = "D:\LiveView\LiveView.Agent\bin\Release\net462"
+$psexecPath = "C:\PsExec\psexec.exe"
+$processName = "LiveView.Agent.exe"
+
+$processCheck = & $psexecPath -accepteula \\$machine -u $username -p $password tasklist | Select-String $processName
+if ($processCheck) {
+    & $psexecPath -accepteula \\$machine -u $username -p $password taskkill /IM $processName /F
+}
+& $psexecPath \\$machine -u $username -p $password "$LiveViewAgentLocation\$processName"
